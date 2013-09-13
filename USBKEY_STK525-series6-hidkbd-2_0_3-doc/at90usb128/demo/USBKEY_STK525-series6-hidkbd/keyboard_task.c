db3ee2cebbd5834a2fdea73b3b18ddfd74282315
gnrr <hideaki.sh@gmail.com>
2013-09-13 14:15:25 +0900

    mod: arrow up/down for powerpoint
    PowerPointプレゼン用に矢印キー上/下に変更

--- USBKEY_STK525-series6-hidkbd-2_0_3-doc/at90usb128/demo/USBKEY_STK525-series6-hidkbd/keyboard_task.c
+++ USBKEY_STK525-series6-hidkbd-2_0_3-doc/at90usb128/demo/USBKEY_STK525-series6-hidkbd/keyboard_task.c
@@ -69,6 +69,7 @@ HID_W, HID_E, HID_L, HID_C, HID_O, HID_M, HID_E, HID_SPACEBAR, HID_T, HID_O, HID
 
 #define SIZEOF_USB_KEYS     (Uint16)sizeof(usb_keys)
 
+U8 code usb_arrow[] = {HID_UP, HID_DOWN};
 U8 code usb_vol[] = {HID_VOLUME_MUTE, HID_VOLUME_UP, HID_VOLUME_DOWN};
 U8 code usb_test[] = {HID_A, HID_B, HID_C};
 
@@ -146,16 +147,27 @@ void keyboard_task(void)
    }
 }
 
-U8 keystate[] = {0, 0, 0, 0};  // 0:initial    1:pressed    2:released
-#define KS_DOWN   0
-#define KS_RIGHT  1
-#define KS_LEFT   2
-#define KS_DUMMY  3
+U8 keystate[] = {0, 0, 0, 0, 0};  // 0:initial    1:pressed    2:released
+#define KS_UP     0
+#define KS_DOWN   1
+#define KS_RIGHT  2
+#define KS_LEFT   3
+#define KS_DUMMY  4
 U8 ksnum;
 bool is_pressed_down()
 {
     U8 *pks = &keystate[KS_DOWN];
     U8 k = Is_joy_down();
+    if((k == TRUE  ) && (*pks == 0)) *pks = 1;
+    if((k == FALSE ) && (*pks == 1)) *pks = 2;
+
+    return (*pks == 2);
+}
+
+bool is_pressed_up()
+{
+    U8 *pks = &keystate[KS_UP];
+    U8 k = Is_joy_up();
     if((k == TRUE  ) && (*pks == 0)) *pks = 1;
     if((k == FALSE ) && (*pks == 1)) *pks = 2;
 
@@ -197,6 +209,22 @@ void kbd_test_hit(void)
       //      usb_data_to_send = SIZEOF_USB_KEYS;
       //      ksnum = KS_DUMMY;
       //  }
+#if 1
+          // arrow keys
+          if(is_pressed_left()) {
+              usb_kbd_state = 1;
+              usb_key_pointer = &usb_arrow[1];
+              usb_data_to_send = 1;
+              ksnum = KS_LEFT;
+          }
+          else if(is_pressed_right()) {
+              usb_kbd_state = 1;
+              usb_key_pointer = &usb_arrow[0];
+              usb_data_to_send = 1;
+              ksnum = KS_RIGHT;
+          }
+#else
+          // volume up/down, mute
           if(is_pressed_down()) {
               usb_kbd_state = 1;
               usb_key_pointer = &usb_vol[0];
@@ -215,6 +243,7 @@ void kbd_test_hit(void)
               usb_data_to_send = 1;
               ksnum = KS_LEFT;
           }
+#endif
           break;
 
       case 1:
